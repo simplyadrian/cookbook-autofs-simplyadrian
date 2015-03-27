7784367020e89cbb41d79e2c5b79ce438aab50dc
simplyadrian <simplyadrian@gmail.com>
2015-03-27 18:21:24 -0500

    ISE-329 syntax correction for attributes file. Added missing comma.

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,7 @@
 default['autofs-nativex']['server'] = "PAE1AL-NAS-03-prv.nativexintern.com"
 # mapper options
 default['autofs-nativex']['maps'] = [{:mount_dir => '/home',
-									  :key => 'TEAMFREEZE'
+									  :key => 'TEAMFREEZE',
 									  :source => '/etc/auto.TEAMFREEZE',
 									  :options => '-rw,intr,rsize=8192,wsize=8192',
 									  :export => '/mnt/ebs'}]
