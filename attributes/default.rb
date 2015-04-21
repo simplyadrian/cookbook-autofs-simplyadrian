78338766292c20f4e4c4e17af9f10a33726d1c20
simplyadrian <simplyadrian@gmail.com>
2015-04-21 12:34:17 -0500

    ISE-329 update to nas fqdn

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,11 +1,13 @@
 # US-East NAS FQDN
-default['autofs-nativex']['us-east1'] = "PAE1AL-NAS-03-prv.nativexintern.com"
+default['autofs-nativex']['us-east1'] = "PAE1AL-NAS-03.teamfreeze.com"
 # US-West1 NAS FQDN
-default['autofs-nativex']['us-west1'] = "PAW1AL-NAS-01-prv.nativexintern.com"
+default['autofs-nativex']['us-west1'] = "PAW1AL-NAS-01.teamfreeze.com"
 # US-West2 NAS FQDN
-default['autofs-nativex']['us-west2'] = "PAW2AL-NAS-02-prv.nativexintern.com"
+default['autofs-nativex']['us-west2'] = "PAW2AL-NAS-02.teamfreeze.com"
+# CDC NAS FQDN
+defaultnode['autofs-nativex']['cdc'] = "NAS.teamfreeze.com"
 # Default value for NAS FQDN
-default['autofs-nativex']['server'] = "PAE1AL-NAS-03-prv.nativexintern.com"
+default['autofs-nativex']['server'] = "PAE1AL-NAS-03.teamfreeze.com"
 # mapper options
 default['autofs-nativex']['maps'] = [{:mount_dir => '/home',
 									  :key => 'TEAMFREEZE',
