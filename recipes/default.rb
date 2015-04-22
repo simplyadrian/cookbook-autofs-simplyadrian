1636f8188e99aec4dab544f3c264bc6d21ed0300
Jesse Hauf <jesse.hauf@nativex.com>
2015-04-22 15:05:10 -0500

    Only run if PBIS is not installed

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,39 +8,46 @@
 #
 
 # If we're in EC2, then need to dynamically determine the NAS based on region
-include_recipe 'autofs-nativex::dynamic_nas' if ec2?
 
-# Install autofs for mounting home directories.
-case node[:platform]
-when 'debian','ubuntu'
-  package 'autofs-ldap'
-  package 'ldap-utils'
-when 'redhat','centos','fedora','amazon'
-  package 'autofs'
+if File.directory?('/etc/pbis')
+  Chef::Log.error('PBIS is installed, skipping adcli/SSSD domain configuration.')
 else
-  Chef::Log.warn('Unknown platform! autofs must be installed manually.')
-end
 
-# Reload autofs service
-service "autofs" do
-  supports [ :status => true, :start => true, :stop => true, :restart => true, :reload => true ]
-  action [:enable, :start]
-end
+  include_recipe 'autofs-nativex::dynamic_nas'
 
-template "/etc/auto.master" do
-  source "auto.master.erb"
-  owner "root"
-  group "root"
-  mode "0644"
-  notifies :reload, resources(:service => "autofs"), :immediately
-end
+  # Install autofs for mounting home directories.
+  case node[:platform]
+  when 'debian','ubuntu'
+    package 'autofs-ldap'
+    package 'ldap-utils'
+  when 'redhat','centos','fedora','amazon'
+    package 'autofs'
+  else
+    Chef::Log.warn('Unknown platform! autofs must be installed manually.')
+  end
 
-node['autofs-nativex']['maps'].each do |map|
-  template map[:source] do
+  # Reload autofs service
+  service "autofs" do
+    supports [ :status => true, :start => true, :stop => true, :restart => true, :reload => true ]
+    action [:enable, :start]
+  end
+
+  template "/etc/auto.master" do
+    source "auto.master.erb"
     owner "root"
     group "root"
-    mode 0644
-    source "auto.map.erb"
+    mode "0644"
     notifies :reload, resources(:service => "autofs"), :immediately
   end
+
+  node['autofs-nativex']['maps'].each do |map|
+    template map[:source] do
+      owner "root"
+      group "root"
+      mode 0644
+      source "auto.map.erb"
+      notifies :reload, resources(:service => "autofs"), :immediately
+    end
+  end
+
 end
